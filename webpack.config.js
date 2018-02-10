804982b5625e2235881acee20928ebc4fe0fd301
elanonimo <bnnm1234@mail.ru>
2018-02-10 20:20:23 +0300

    added CSSExtract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,44 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry : './src/app.js',
-	output: {
-		path    : path.join(__dirname, 'public'),				// D:\Complete React Mead\indecision-app\public
-		filename: 'bundle.js'
-	},
-	module : {
-		rules: [{
-			loader : 'babel-loader',
-			test   : /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use : ['style-loader', 'css-loader', 'sass-loader']
-		}]
-	},
-	devtool  : 'cheap-module-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	},
+module.exports = (env) => {
+	console.log('env:', env);
+
+	const isProduction = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('style.css');
+
+	return {
+		entry : './src/app.js',
+		output: {
+			path    : path.join(__dirname, 'public'),				// D:\Complete React Mead\indecision-app\public
+			filename: 'bundle.js'
+		},
+		module : {
+			rules: [{
+				loader : 'babel-loader',
+				test   : /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				// use : ['style-loader', 'css-loader', 'sass-loader']
+				use: CSSExtract.extract({
+					use: [
+						{
+							loader: 'css-loader', options: { sourceMap: true }
+						}, {
+							loader: 'sass-loader', options: { sourceMap: true }
+						}
+					]
+				})
+			}]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool  : isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		},
+	}
 };
